86d43e40f944ffebe6273cc50ed5a72adba84721
InuSei <0320-2084@lspu.edu.ph>
2026-01-08 21:11:24 +0800

    I DID IT i think

--- frontend/src/api/config.jsx
+++ frontend/src/api/config.jsx
@@ -1,5 +1,5 @@
 // src/api/config.js
-export const API_BASE_URL = "https://lavona-orthodox-leota.ngrok-free.dev";
+export const API_BASE_URL = "beneficial-determination-production-2c43.up.railway.app";
 
 export const getHeaders = (token) => ({
   "Content-Type": "application/json",
