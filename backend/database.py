0fe6a7032ef3194081772a45d82a815371786d00
InuSei <0320-2084@lspu.edu.ph>
2026-01-08 19:37:47 +0800

    database?

--- backend/database.py
+++ backend/database.py
@@ -1,14 +1,15 @@
-
 import mysql.connector
 from mysql.connector import Error
 
 def get_db_connection():
     try:
+        # REPLACE THESE with your actual Aiven details
         connection = mysql.connector.connect(
-            host="localhost",
-            user="root",
-            password="@120302#091703!!123Anton", # Consider using environment variables for safety!
-            database="elderly_health_db"
+            host="elderly-health-db-lspu-8527.f.aivencloud.com", # From Aiven
+            user="avnadmin",                           # From Aiven
+            password="AVNS_UzvSw11mmPQ4ZKOjDYf",       # From Aiven
+            database="defaultdb",                      # Usually 'defaultdb' on Aiven
+            port=27003                                 # From Aiven
         )
         return connection
     except Error as e:
