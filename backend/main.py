3d4e428143e317f794f60b65ffda501dd2db37f8
InuSei <0320-2084@lspu.edu.ph>
2026-01-08 23:36:35 +0800

    asd

--- backend/main.py
+++ backend/main.py
@@ -2,17 +2,12 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from backend.routers import auth, devices, vitals, users, notifications
 
-app = FastAPI(title="Elderly Health Monitoring API")
+app = FastAPI(title="Elderly Health Monitoring API", redirect_slashes=False)
 
-origins = [
-    "https://elder-health-monitor.vercel.app", # Your main Vercel link
-    "http://localhost:5173", 
-    "http://localhost:3000"
-]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials = True,
     allow_methods=["*"],
     allow_headers=["*"],
